3d44a1d8aead062a21e17846f55600ecaffcac48
lim <569212066@qq.com>
2020-12-17 16:42:35 +0800

    feat: add fibonacci test

---
+++ lesson/test/main.test.js
@@ -0,0 +1,36 @@
+var main = require('../main');
+var should = require('should');
+
+describe('test/main.test.js', () => {
+  it('should equal 55 when n === 10', ()=> {
+    main.fibonacci(10).should.equal(55);
+  });
+
+  it('should equal 0 when n === 0', ()=> {
+    main.fibonacci(0).should.equal(0);
+  });
+
+  it('should equal 1 when n === 1', ()=> {
+    main.fibonacci(1).should.equal(1);
+  });
+
+  // it('should thro when n > 10', ()=> {
+  //   (()=> {
+  //     main.fibonacci(10)
+  //   }).should.throw('n should <= 10');
+    
+  // })
+
+  // it('should throw when n < 0', function () {
+  //   (()=> {
+  //     main.fibonacci(-1);
+  //   }).should.throw('n should >= 0');
+  // });
+  
+  it('should throw when n is not Number', function () {
+    (()=> {
+      main.fibonacci('中文');
+    }).should.throw('n should be a number')
+  });
+});
+
